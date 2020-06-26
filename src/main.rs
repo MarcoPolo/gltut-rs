a19d906e9121d84d8382a1100401d6f65388ab41
Marco Munizaga <git@marcopolo.io>
2020-06-25 17:32:29 -0700

    Match against args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use glfw::{Action, Context, Key};
 mod ch_1;
+mod ch_2;
+mod ch_3;
+mod shader;
 
 fn main() {
-    ch_1::main_hello_triangle();
+    let args: Vec<String> = std::env::args().collect();
+    println!("Args: {:?}", args);
+    match args[1].as_str() {
+        "ch_1" => ch_1::main_hello_triangle(),
+        "ch_2" => ch_2::main_hello_color(),
+        "ch_2_va" => ch_2::vertex_attributes::main(),
+        "ch_3" => ch_3::part1::main(),
+        _ => println!("Unimplemented"),
+    }
 }
